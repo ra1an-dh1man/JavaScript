049fb57d8bfc481ead24ff0e7dfcfe8e8f9b329d
Rajan Dhiman <dhimanrajan7252@gmail.com>
2024-06-25 16:21:35 +0530

    Create for_of.js

---
+++ for_of.js
@@ -0,0 +1,27 @@
+let a = "Mayank"
+
+for(let i of a)
+    {
+        console.log(i);  
+    }
+    /*
+    M
+    a
+    y
+    a
+    n
+    k
+    */
+
+for(let i in a)
+    {
+        console.log(i);
+    }
+    /*
+    0
+    1
+    2
+    3
+    4
+    5
+    */
