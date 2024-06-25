3fc0b3f7af7db10b6d74cac285ca7afa6087fa53
Rajan Dhiman <dhimanrajan7252@gmail.com>
2024-06-25 17:36:25 +0530

    Create template_literal.js

---
+++ template_literal.js
@@ -0,0 +1,9 @@
+const stud = {
+    name: "Maynka",
+    age : 45,
+    isPassed : true,
+    marks : 69
+}
+
+console.log(`Student name ${stud.name} of ${stud.age} year, got ${stud.marks} marks`);
+console.log(`Passed ?? ${stud.isPassed}`);
