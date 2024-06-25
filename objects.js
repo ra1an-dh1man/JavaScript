3c9edb6b0359882130696e30a53119c5b8914fc7
Rajan Dhiman <dhimanrajan7252@gmail.com>
2024-06-25 15:12:41 +0530

    Create objects.js

---
+++ objects.js
@@ -0,0 +1,11 @@
+const pen = {
+    name: "Parker Jotter Standard CT Ball Pen",
+    price: "₹270",
+    isDeal: true,
+    rating: "4/5",
+    discount: "5%"
+};
+
+Object.freeze(pen)
+
+console.log(pen);
