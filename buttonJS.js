e00d3e67bd11373d4ced997a2043391870223eaf
Rajan Dhiman <dhimanrajan7252@gmail.com>
2024-06-27 18:50:15 +0530

    Create buttonJS.js

---
+++ buttonJS.js
@@ -0,0 +1,7 @@
+let newBtn = document.createElement("button");
+newBtn.innerText = "click me!!";
+
+newBtn.style.backgroundColor = "red";
+newBtn.style.color = "white";
+newBtn.style.borderRadius = "20px"
+document.querySelector("body").prepend(newBtn);
